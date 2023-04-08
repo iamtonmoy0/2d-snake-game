294ed1093649664df614442581aba53db902e8f1
iamtonmoy0 <tonmoy2code@gmail.com>
2023-04-08 07:57:08 -0400

    snake food added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-console.log('hello')
+const gameBoard=document.getElementById('game-board')
 
-
+let scaleX , scaleY;
+// food  position change 
+const snakeFood=()=>{
+	scaleX= Math.floor(Math.random()* 30) + 1;
+	scaleY= Math.floor(Math.random()* 30) + 1;
+	console.log(scaleX, scaleY)
+}
+const initGame=()=>{
+	let foodHtml=`<div id="food" style="grid-area:${scaleY}/${scaleX}"> </div>`;
+          gameBoard.innerHTML=foodHtml;
+}
+snakeFood()
+initGame()
