4d7bc26493aa3022deec355910f520cdedb2d656
iamtonmoy0 <tonmoy2code@gmail.com>
2023-04-09 04:16:42 -0400

    snake movement added

--- js/script.js
+++ js/script.js
@@ -2,17 +2,38 @@ const gameBoard=document.getElementById('game-board')
 
 let scaleX , scaleY;
 let snakeX=10,snakeY=16;
+// velocity
+let velocityX=0,velocityY=0;
 // food  position change 
 const snakeFood=()=>{
 	scaleX= Math.floor(Math.random()* 30) + 1;
 	scaleY= Math.floor(Math.random()* 30) + 1;
 	// console.log(scaleX, scaleY)
 }
+const changeDir=(e)=>{
+	if(e.key==="ArrowUp"){
+		velocityX=0;
+		velocityY=-1;
+	}else if(e.key==="ArrowDown"){
+		velocityX=0;
+		velocityY=1;
+	}else if(e.key==="ArrowRight"){
+		velocityX=1;
+		velocityY=0;
+	}if(e.key==="ArrowLeft"){
+		velocityX=-1;
+		velocityY=0;
+	}
+
+}
 const initGame=()=>{
 	let foodHtml=`<div id="food" style="grid-area:${scaleY}/${scaleX}"> </div>`;
-   foodHtml+=`<div id="snake" style="grid-area:${snakeY}/${snakeX}"> </div>`;
+ snakeX+=velocityX;
+ snakeY+=velocityY;  
+	foodHtml+=`<div id="snake" style="grid-area:${snakeY}/${snakeX}"> </div>`;
      
 	gameBoard.innerHTML=foodHtml;
 }
 snakeFood()
-initGame()
+setInterval(initGame,125)
+document.addEventListener("keydown",changeDir)
